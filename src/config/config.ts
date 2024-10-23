9447eac767bd83b76ce465e42279b33e851ab57e
MartotoBG <martingav123@gmail.com>
2024-10-23 15:09:24 +0300

    creating a zbd token

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,4 +3,6 @@ import * as path from "path";
 const envPath = path.resolve(__dirname, '../../.env');
 dotenv.config({ path: envPath });
 const botToken = process.env["bot-token"];
-export { botToken }
+const zbdToken = process.env["zbd-token"];
+const env = process.env;
+export { botToken, zbdToken, env }
