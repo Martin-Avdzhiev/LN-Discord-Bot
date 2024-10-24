21536dee86119ee2c7132fa3a737b3f2c8431a4d
MartotoBG <martingav123@gmail.com>
2024-10-24 15:57:29 +0300

    Update lightning.ts

--- src/lightning/lightning.ts
+++ src/lightning/lightning.ts
@@ -1,18 +1,29 @@
 import { zbd } from '@zbd/node';
 import { zbdToken } from '../config/config';
+import { generateQRCode } from '../QR-code/qrGenerator';
 // Instantiate ZBD client
 async function createCharge() {
     if (zbdToken) {
         const ZBD = new zbd(zbdToken);
-        
+
+
+
         // Creating a payload for the charge
         const payload = {
             expiresIn: 300, // seconds
             amount: '50000', // millisatoshis (1 satoshi = 1000 millisatoshis)
-            internalId: '118304b8',
-            description: 'My Charge Description',
-            callbackUrl: 'https://your-app.com/zbd-callback',
+            internalId: '311176904726282242', // Discord User ID
+            description: 'Paymen for role',
+            callbackUrl: 'https://your-app.com/zbd-callback'
         };
+
+        // Options for the request
+        const options = {
+            method: 'POST',
+            headers: {apikey: zbdToken, 'Content-Type': 'application/json'},
+            body: JSON.stringify(payload)
+          };
+          
 
         // Creating a charge
         try {
